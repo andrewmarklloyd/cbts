0bdd8fcd2a503c82e7cc9823de8a00d67d91fafa
Andrew Lloyd <andrewlloyd@Andrews-MacBook-Pro.local>
2020-09-11 09:50:56 -0700

    Add synced folder, add install output

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+
+echo "Installing homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+echo "Installing virtualbox"
 brew cask install virtualbox
+
+echo "Installing vagrant"
 brew cask install vagrant
 
+echo "Downloading Vagrantfile"
 curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/andrewmarklloyd/cbts/master/Vagrantfile > Vagrantfile
 
+echo "Starting virtual machine"
 vagrant up
